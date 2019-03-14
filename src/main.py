6f7a4246771335394db7ee57b3da0f244be9a177
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-03-13 23:04:44 -0700

    Inital display of blocks

--- src/main.py
+++ src/main.py
@@ -5,7 +5,13 @@ from entity import Entity
 TITLE = "Breakout"
 
 BLACK = (0, 0, 0)
+GREY = (142, 142, 142)
 RED = (200, 72, 72)
+ORANGE = (198, 108, 58)
+BROWN = (180, 122, 48)
+YELLOW = (162, 162, 42)
+GREEN = (72, 160, 72)
+BLUE = (66, 72, 200)
 
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 600
@@ -15,6 +21,15 @@ PLAYER_HEIGHT = 20
 PLAYER_X = WINDOW_WIDTH / 2 - PLAYER_WIDTH / 2
 PLAYER_Y = 550
 PLAYER_VELOCITY = 10
+
+BLOCK_WIDTH = 80
+BLOCK_HEIGHT = 20
+NUM_BLOCKS_X = 9
+NUM_BLOCKS_Y = 6
+
+WALL_SIZE = 40
+
+FPS = 60
 
 
 def main():
@@ -29,6 +44,33 @@ def main():
     vel = PLAYER_VELOCITY
 
     player = Entity(PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT, RED)
+    blocks = []
+    for row in range(0, NUM_BLOCKS_Y):
+        for col in range(0, NUM_BLOCKS_X):
+            block_x = col * BLOCK_WIDTH
+            block_y = row * BLOCK_HEIGHT
+            block_color = None
+            if row == 0:
+                block_color = RED
+            elif row == 1:
+                block_color = ORANGE
+            elif row == 2:
+                block_color = BROWN
+            elif row == 3:
+                block_color = YELLOW
+            elif row == 4:
+                block_color = GREEN
+            else:
+                block_color = BLUE
+            blocks.append(
+                Entity(
+                    WALL_SIZE + block_x,
+                    WALL_SIZE * 2 + 48 + block_y,
+                    BLOCK_WIDTH,
+                    BLOCK_HEIGHT,
+                    block_color,
+                )
+            )
 
     while running:
         for event in pygame.event.get():
@@ -47,8 +89,11 @@ def main():
 
         screen.fill(BLACK)
         player.render(screen)
+        for block in blocks:
+            block.render(screen)
         pygame.display.flip()
-        clock.tick(60)
+
+        clock.tick(FPS)
 
 
 if __name__ == "__main__":
