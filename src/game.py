d0d9411673f65438d2b168af3f061a86348f55e3
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-03-15 20:54:22 -0700

    Refactored entities into Game class

--- src/game.py
+++ src/game.py
@@ -1,14 +1,32 @@
 import pygame
 
+from ball import Ball
+from block import init_blocks
 from colors import BLACK
+from player import Player
+from wall import init_walls
 
 
-def render_game(screen, player, ball, blocks, walls):
-    screen.fill(BLACK)
-    player.render(screen)
-    ball.render(screen)
-    for block in blocks:
-        block.render(screen)
-    for wall in walls:
-        wall.render(screen)
-    pygame.display.flip()
+class Game:
+    def __init__(self):
+        self.player = Player()
+        self.ball = Ball()
+        self.walls = init_walls()
+        self.blocks = init_blocks()
+
+    def update(self, action):
+        move = action.get("move")
+        launch = action.get("launch")
+
+        self.player.update(move, self.walls)
+        self.ball.update(launch, self.player, self.blocks, self.walls)
+
+    def render(self, screen):
+        screen.fill(BLACK)
+        self.player.render(screen)
+        self.ball.render(screen)
+        for block in self.blocks:
+            block.render(screen)
+        for wall in self.walls:
+            wall.render(screen)
+        pygame.display.flip()
--- src/main.py
+++ src/main.py
@@ -1,11 +1,7 @@
 import pygame
 
-from ball import BALL_VELOCITY_X, BALL_VELOCITY_Y, Ball
-from block import init_blocks
-from game import render_game
+from game import Game
 from input_handlers import handle_keys
-from player import Player
-from wall import init_walls
 from window import init_screen
 
 FPS = 60
@@ -15,11 +11,7 @@ def main():
     pygame.init()
 
     screen = init_screen()
-
-    player = Player()
-    ball = Ball()
-    walls = init_walls()
-    blocks = init_blocks()
+    game = Game()
 
     running = True
     clock = pygame.time.Clock()
@@ -27,17 +19,13 @@ def main():
         keys = pygame.key.get_pressed()
         action = handle_keys(keys)
 
-        move = action.get("move")
-        launch = action.get("launch")
         quit = action.get("quit")
 
         if quit:
             running = False
 
-        player.update(move, walls)
-        ball.update(launch, player, blocks, walls)
-
-        render_game(screen, player, ball, blocks, walls)
+        game.update(action)
+        game.render(screen)
 
         clock.tick(FPS)
 
