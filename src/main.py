b98ea989bac2ba0e2206b378b5daa5c34c90448b
Daniel Gomez <47473031+0xbadd@users.noreply.github.com>
2019-07-02 19:35:38 -0700

    Implemented play_game function in main

--- src/main.py
+++ src/main.py
@@ -7,6 +7,32 @@ from window import init_screen
 FPS = 60
 
 
+def play_game(screen, clock):
+    game = Game()
+
+    running = True
+    while running:
+        keys = pygame.key.get_pressed()
+        action = handle_keys(keys)
+        results = {}
+
+        quit = action.get("quit")
+
+        if quit:
+            running = False
+
+        results = game.update(action)
+
+        lost = results.get("lost")
+
+        if lost:
+            game = Game()
+        else:
+            game.render(screen)
+
+        clock.tick(FPS)
+
+
 def main():
     pygame.init()
 
